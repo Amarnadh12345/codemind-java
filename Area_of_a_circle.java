cbc4d27cf3955e25c4baaac43b93023df677feac
Amarnadh12345 <81435460+Amarnadh12345@users.noreply.github.com>
2022-09-23 06:45:53 +0530

    import java.util.*; import java.lang.*; class a{     public static void main(String args[]){         double c;         int n;         double pi= 3.14;         Scanner sc = new  Scanner(System.in);         n=sc.nextInt();         c=(pi*n*n);         System.out.printf("%.2f",c);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+class a{
+    public static void main(String args[]){
+        double c;
+        int n;
+        double pi= 3.14;
+        Scanner sc = new  Scanner(System.in);
+        n=sc.nextInt();
+        c=(pi*n*n);
+        System.out.printf("%.2f",c);
+    }
+}
