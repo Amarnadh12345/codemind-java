402c9283b03afcb50985b10dcbd038c975a70b0d
Amarnadh12345 <81435460+Amarnadh12345@users.noreply.github.com>
2022-10-30 13:11:06 +0530

    import java.util.*; class a{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         String st=sc.nextLine();         StringBuilder input1= new StringBuilder();         input1.append(st);         input1=input1.reverse();         System.out.println(input1);     } }

---
+++ Reverse_String.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class a{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String st=sc.nextLine();
+        StringBuilder input1= new StringBuilder();
+        input1.append(st);
+        input1=input1.reverse();
+        System.out.println(input1);
+    }
+}
