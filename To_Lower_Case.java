c2ffde95908da1223c5835377e36fd76c8bdcbfa
Amarnadh12345 <81435460+Amarnadh12345@users.noreply.github.com>
2022-10-30 13:40:52 +0530

    import java.util.*; class a{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         String st=sc.nextLine();         String l=st.toLowerCase();         System.out.println(l);     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class a{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String st=sc.nextLine();
+        String l=st.toLowerCase();
+        System.out.println(l);
+    }
+}
